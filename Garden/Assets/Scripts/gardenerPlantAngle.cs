985ca2d23e9a892ef0d6361f413b29baa3fd87f8
smcniel <mcnes430@newschool.edu>
2014-11-03 15:53:19 -0500

    hello worlds.

---
+++ Garden/Assets/Scripts/gardenerPlantAngle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gardenerPlantAngle : MonoBehaviour {
+
+	public Transform treePrefab; // assign in inspector
+	public Transform gardenerPrefab; // assign in inspector
+
+	public float lastYvalue = 0f;
+
+
+
+		void Update () {
+
+		// plant based on when I turn... lookup "transform.eulerAngles" in Unity docs
+			if (Mathf.Abs (transform.eulerAngles.y - lastYvalue) > 15f) {
+
+			 Instantiate (treePrefab, transform.position, Quaternion.identity);
+
+				}
+	
+		lastYvalue = transform.eulerAngles.y;
+		}
+
+		
+	// transform.rotation.eulerAngles );  // transforms Quaternion to euler
+	// Quaternion whatever = Quaternion.Euler( 0f, 180f, 90f);  // transforms euler into quaternion
+}
---
+++ Garden/Assets/Scripts/treeDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class treeDestroy : MonoBehaviour {
+	public Component lumberJack;
+	
+	
+	
+	void OnCollisionEnter (Collision col){
+		lumberJack.tag = "tree";
+		if (col.gameObject.tag == "tree"){
+			Destroy(gameObject); // Not sure why I had to the script on the tree, and not on the lumberJack but this works
+		}
+		
+	}
+}
